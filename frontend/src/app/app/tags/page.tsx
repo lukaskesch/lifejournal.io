aa170e525847c93a4a9f60cb037de6ce3c3805d1
Lukas Kesch <lukas.kesch@gmail.com>
2024-08-14 19:56:43 +0200

    Add route to list tags and add new ones

---
+++ frontend/src/app/app/tags/page.tsx
@@ -0,0 +1,57 @@
+"use server";
+
+import { db } from "../../../../db";
+import { user_tags, users, UserTags } from "../../../../drizzle/schema";
+import { eq } from "drizzle-orm/expressions";
+import { v4 as uuidv4 } from "uuid";
+import TagsClient from "@/components/tags/TagsClient";
+
+export default async function Tags() {
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, "lukas.kesch@gmail.com"))
+    .limit(1)
+    .execute()
+    .then((result) => result[0]);
+
+  async function getUserTags(userId: string) {
+    const userTags = await db
+      .select()
+      .from(user_tags)
+      .where(eq(user_tags.user_id, userId))
+      .execute();
+    return userTags;
+  }
+
+  async function addTagToUser(tagName: string): Promise<UserTags> {
+    "use server";
+    try {
+      const id = uuidv4();
+      await db.insert(user_tags).values({
+        id: id,
+        user_id: user.id,
+        name: tagName,
+      });
+
+      const tags = await db
+        .select()
+        .from(user_tags)
+        .where(eq(user_tags.id, id))
+        .execute();
+      return tags[0];
+    } catch (error) {
+      console.error("Error adding tag to user:", error);
+      throw new Error("Error adding tag to user");
+    }
+  }
+
+  return (
+    <div>
+      <TagsClient
+        initialTags={await getUserTags(user.id)}
+        createTag={addTagToUser}
+      />
+    </div>
+  );
+}
---
+++ frontend/src/components/tags/TagsClient.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React from "react";
+import { User, UserTags } from "../../../drizzle/schema";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export default function TagsClient({
+  initialTags,
+  createTag,
+}: {
+  initialTags: UserTags[];
+  createTag: (name: string) => Promise<UserTags>;
+}) {
+  const [tags, setTags] = React.useState(initialTags);
+  const [newTagName, setNewTagName] = React.useState("");
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+
+  async function handleCreateTagButtonClick() {
+    if (!newTagName) return;
+    const newTag = await createTag(newTagName);
+    setNewTagName("");
+    setTags([...tags, newTag].sort((a, b) => a.name.localeCompare(b.name)));
+    setIsDialogOpen(false);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {tags && tags.map((tag) => <div key={tag.id}>{tag.name}</div>)}
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <div className="m-4">
+            <Button>Add Tag</Button>
+          </div>
+          {/* <button className="btn">Open Dialog</button> */}
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[400px]">
+          <DialogHeader>
+            <DialogTitle>Add a Tag</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col">
+            <div className="my-4">
+              <Input
+                placeholder="Tag Name"
+                value={newTagName}
+                onChange={(e) => setNewTagName(e.target.value)}
+                aria-describedby="input-add-tag-description"
+              />
+              <div id="input-add-tag-description" className="sr-only">
+                Enter the name of the tag you want to add.
+              </div>
+            </div>
+            <div className="self-end mt-4">
+              <Button
+                disabled={!newTagName}
+                onClick={handleCreateTagButtonClick}
+              >
+                Add Tag
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
