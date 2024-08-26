a4a87691a90e5f3134e334c3cb8ebad0f4eac738
Lukas Kesch <lukas.kesch@gmail.com>
2024-08-26 21:02:06 +0200

    Configure next-auth to support cretentials login

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "../../../../../db/index";
+import { users } from "../../../../../drizzle/schema";
+import { eq } from "drizzle-orm";
 
 const handler = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -14,32 +16,58 @@ const handler = NextAuth({
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        return { id: 1, name: "J Smith", email: "jsmith@example.com" };
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
-
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user;
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
         }
-        // Return null if user data could not be retrieved
-        return null;
+
+        const userArray = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.username))
+          .limit(1);
+
+        const user = userArray[0];
+
+        console.log(user);
+        if (!user) {
+          return null;
+        }
+
+        // const isPasswordValid = await bcrypt.compare(
+        //   credentials.password,
+        //   user.password,
+        // );
+        //
+        // if (!isPasswordValid) {
+        //   return null;
+        // }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
       },
     }),
   ],
+  // session: {
+  //   strategy: "database",
+  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+  //   updateAge: 24 * 60 * 60, // 24 hours
+  // },
+  // secret: process.env.NEXTAUTH_SECRET,
+  // callbacks: {
+  //   session: async ({ session, user }) => {
+  //     if (session?.user) {
+  //       session.user.id = user.id;
+  //     }
+  //     return session;
+  //   },
+  // },
 });
 
 export { handler as GET, handler as POST };
