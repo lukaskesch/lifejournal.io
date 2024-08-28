ab4b0537feb6a84202407b5a967901208a6b7ea3
Lukas Kesch <lukas.kesch@gmail.com>
2024-08-28 20:12:34 +0200

    Change header whether user is logged in or not

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,27 +1,41 @@
 import Link from "next/link";
-export default function Header() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+
+export default async function Header() {
+  const session = await getServerSession(authOptions);
   return (
     <header className="bg-primary text-white p-4">
-      <nav className="container mx-auto flex justify-between items-center">
+      <nav className=" flex justify-between items-center">
         <Link href="/" className="text-xl font-bold">
           My Focus Journal
         </Link>
         <ul className="flex space-x-4">
-          <li>
-            <Link href="/app" className="hover:text-gray-300">
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link href="/app/logs" className="hover:text-gray-300">
-              Logs
-            </Link>
-          </li>
-          <li>
-            <Link href="/app/tags" className="hover:text-gray-300">
-              Tags
-            </Link>
-          </li>
+          {session ? (
+            <>
+              <li>
+                <Link href="/app" className="hover:text-gray-300">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link href="/app/logs" className="hover:text-gray-300">
+                  Logs
+                </Link>
+              </li>
+              <li>
+                <Link href="/app/tags" className="hover:text-gray-300">
+                  Tags
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link href="/api/auth/signin" className="hover:text-gray-300">
+                Sign in
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
