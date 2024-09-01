27f3c355f870c4bcdc6025e3e0ddad6396a38614
Lukas Kesch <lukas.kesch@gmail.com>
2024-09-01 14:54:38 +0200

    Writing own tag selection UI
    Now it is responsive

--- frontend/src/components/logging/SelectTagsClient.tsx
+++ frontend/src/components/logging/SelectTagsClient.tsx
@@ -11,20 +11,33 @@ export default function SelectTagsClient({
   setSelectedTagsIds: (tags: string[]) => void;
 }) {
   userTags.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
-    <div>
-      <ToggleGroup
-        type="multiple"
-        value={selectedTagsIds}
-        onValueChange={setSelectedTagsIds}
-      >
-        {userTags.map((tag) => (
-          <ToggleGroupItem key={tag.id} value={tag.id}>
-            {"#" + tag.name}
-          </ToggleGroupItem>
-        ))}
-      </ToggleGroup>
-      <ul></ul>
+    <div className="flex flex-row flex-wrap">
+      {userTags.map((tag) => (
+        <label
+          key={tag.id}
+          className={`${selectedTagsIds.some((id) => id === tag.id) && "bg-slate-200"} m-1 p-2 rounded-lg cursor-pointer`}
+        >
+          <input
+            type="checkbox"
+            className="sr-only"
+            checked={selectedTagsIds.includes(tag.id)}
+            onChange={(e) => {
+              if (e.target.checked) {
+                setSelectedTagsIds([...selectedTagsIds, tag.id]);
+              } else {
+                setSelectedTagsIds(
+                  selectedTagsIds.filter(
+                    (selectedTagId) => selectedTagId !== tag.id,
+                  ),
+                );
+              }
+            }}
+          />
+          {"#" + tag.name}
+        </label>
+      ))}
     </div>
   );
 }
