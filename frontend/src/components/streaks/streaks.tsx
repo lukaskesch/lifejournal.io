3dc9694e1db48b688b9a4f16ea19451163b68e65
Lukas Kesch <lukas.kesch@gmail.com>
2024-10-29 16:37:28 +0100

    Add mindfulness streak

--- frontend/src/components/streaks/streaks.tsx
+++ frontend/src/components/streaks/streaks.tsx
@@ -9,10 +9,9 @@ export default async function Streaks({}) {
         // startDate={new Date(new Date().setMonth(new Date().getMonth() - 1))}
       />
       {/* Software Development */}
-
-      <Streak
-        tagId="32a49b2e-d0b1-442f-8a45-a2f41bce6379"
-      />
+      <Streak tagId="32a49b2e-d0b1-442f-8a45-a2f41bce6379" />
+      {/* Mindfulness */}
+      <Streak tagId="823c9a67-454d-4cac-b13f-93e676cc032c" />
     </div>
   );
 }
