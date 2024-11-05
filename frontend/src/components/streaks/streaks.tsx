cce70cbbebd0ac5065853f23bb9767b47eb57ee2
Lukas Kesch <lukas.kesch@gmail.com>
2024-11-05 19:56:17 +0100

    Add reading streak

--- frontend/src/components/streaks/streaks.tsx
+++ frontend/src/components/streaks/streaks.tsx
@@ -12,6 +12,8 @@ export default async function Streaks({}) {
       <Streak tagId="32a49b2e-d0b1-442f-8a45-a2f41bce6379" />
       {/* Mindfulness */}
       <Streak tagId="823c9a67-454d-4cac-b13f-93e676cc032c" />
+      {/* Reading */}
+      <Streak tagId="86ee71f4-2c84-4eab-a056-28883bad352c" />
     </div>
   );
 }
