bd390af34ec3f3f59c2746daea07c343e6dd1edd
Lukas Kesch <lukas.kesch@gmail.com>
2025-03-23 11:16:08 +0100

    When sign out direct user to homepage

--- src/components/ui/SignOutButton.tsx
+++ src/components/ui/SignOutButton.tsx
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 export default function SignOutButton() {
   return (
     <button
-      onClick={() => signOut()}
+      onClick={() => signOut({ callbackUrl: "/" })}
       className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors"
     >
       Sign out
